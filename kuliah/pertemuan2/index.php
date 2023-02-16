13a30e3c8aa8b6ded3f110f1f1cc540216fe832b
azharutama <azharutama877@gmail.com>
2023-02-16 16:02:44 +0700

    tugas 1 pemrograman web

---
+++ kuliah/pertemuan2/index.php
@@ -0,0 +1,23 @@
+<?php
+  $nama = 'azhar';
+  $matkul = 'pemrograman web';
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>pertemuan2</title>
+</head>
+<body>
+  
+  <h1>
+    <?php echo " Hello, $nama"?>  
+  </h1>
+  
+  
+  <h2><?php echo $matkul ?></h2>
+  
+</body>
+</html>
---
+++ tugas/tugas1/1b.php
@@ -0,0 +1,43 @@
+<?php 
+  $x = "77";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+  <?php 
+
+  echo "aku adalah angka <b> $x </b> </br>";
+
+  //perkalian
+  $kali = $x * 5;
+  echo " jika aku dikali 5, maka aku sekarang menjadi <b>$kali</b> </br> ";
+
+  
+
+ //pembagian
+ $bagi = $x / 2;
+ echo " jika aku dibagi 2, maka aku sekarang menjadi <b>$bagi</b> </br> ";
+
+ //pembagian
+ $tambah = $x + 75;
+ echo " jika aku ditambah 75, maka aku sekarang menjadi <b>$tambah</b> </br> ";
+
+ //pembagian
+ $kurang = $x - 20;
+ echo " jika aku dikurang 20, maka aku sekarang menjadi <b>$kurang</b> </br> ";
+
+
+
+
+?>
+  
+  
+</body>
+</html>
